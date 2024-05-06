b235f07c1fdc67e257d0ba34c41bfd6378489689
Antonio-M94 <imondragonespinal@gmail.com>
2024-05-05 23:16:26 -0600

    Primera version de iamedev-weather-app

---
+++ README.md
@@ -0,0 +1,37 @@
+# Weather App
+
+Este proyecto es una aplicación web simple para consultar el clima, desarrollada por IAMEDEV. Permite a los usuarios verificar las condiciones climáticas de una ciudad específica o utilizar su ubicación actual para obtener el pronóstico del tiempo.
+
+## Características
+
+- **Búsqueda por Ciudad:** Los usuarios pueden ingresar el nombre de una ciudad en el campo de entrada y buscar su pronóstico del tiempo.
+- **Usar Ubicación Actual:** Los usuarios pueden optar por utilizar la ubicación actual de su dispositivo para obtener el pronóstico del tiempo.
+- **Pronóstico de 5 Días:** La aplicación proporciona un pronóstico del tiempo de 5 días que incluye temperatura, velocidad del viento y humedad.
+- **Diseño Responsivo:** La aplicación está diseñada para funcionar sin problemas en varios dispositivos y tamaños de pantalla.
+
+## Cómo Usar
+
+1. Ingresa el nombre de la ciudad en el campo de entrada.
+2. Haz clic en el botón "Buscar" para obtener el pronóstico del tiempo para la ciudad ingresada.
+3. Alternativamente, haz clic en el botón "Usar Ubicación Actual" para obtener el pronóstico del tiempo basado en la ubicación actual de tu dispositivo.
+4. Los detalles del clima actual y un pronóstico de 5 días se mostrarán debajo del campo de entrada.
+
+## Tecnologías Utilizadas
+
+- **HTML:** Lenguaje de marcado para estructurar la página web.
+- **CSS:** Lenguaje de estilos utilizado para diseñar la interfaz de usuario.
+- **JavaScript:** Lenguaje de programación utilizado para el comportamiento dinámico e interacciones.
+- **OpenWeatherMap API:** Utilizada para obtener datos meteorológicos basados en el nombre de la ciudad o coordenadas.
+
+## Configuración
+
+Para ejecutar este proyecto localmente, sigue estos pasos:
+
+1. Clona este repositorio en tu máquina local.
+2. Abre el archivo `index.html` en tu navegador web.
+
+## Créditos
+
+- **IAMEDEV:** Desarrolló la aplicación Weather App.
+- **OpenWeatherMap:** Proporcionó los datos meteorológicos a través de su API.
+
---
+++ js/script.js
@@ -0,0 +1,137 @@
+// Seleccionar elementos del DOM
+const cityInput = document.querySelector('.city-input');
+const searchButton = document.querySelector('.search-btn');
+const locationButton = document.querySelector('.location-btn');
+const currentWeatherDiv = document.querySelector('.current-weather');
+const weatherCardsDiv = document.querySelector('.weather-cards');
+
+// Clave de la API de OpenWeatherMap
+const API_KEY = '24a01dab159a73a155df15cb9edaddfb';
+
+// Función para crear tarjetas de clima
+const createWeatherCard = (cityName, weatherItem, index) => {
+  if (index === 0) {
+    // Tarjeta para el clima actual
+    return `
+    <div class="details">
+    <h2>${cityName} (${weatherItem.dt_txt.split(' ')[0]})</h2>
+    <h4>Temperature: ${weatherItem.main.temp.toFixed(2)}°C</h4>
+    <h4>Wind: ${weatherItem.wind.speed.toFixed(2)} M/S</h4> 
+    <h4>Humidity: ${weatherItem.main.humidity}%</h4>
+    </div>
+    <div class="icon">
+    <img src="https://openweathermap.org/img/wn/${
+      weatherItem.weather[0].icon
+    }@4x.png" alt="Weather-icon">
+    <h4>${weatherItem.weather[0].description}</h4>
+    </div>`;
+  } else {
+    // Tarjeta para el clima futuro
+    return `<li class="card">
+  <h3>(${weatherItem.dt_txt.split(' ')[0]})</h3>
+  <img src="https://openweathermap.org/img/wn/${
+    weatherItem.weather[0].icon
+  }@2x.png" alt="Weather-icon">
+  <h4>Temp: ${weatherItem.main.temp.toFixed(2)}°C</h4>
+  <h4>Wind: ${weatherItem.wind.speed.toFixed(2)} M/S</h4>
+  <h4>Humidity: ${weatherItem.main.humidity}%</h4>
+</li>`;
+  }
+};
+
+// Función para realizar solicitudes fetch con manejo de errores
+const fetchData = async (url) => {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+};
+
+// Función para obtener detalles del clima
+const getWeatherDetails = async (cityName, lat, lon) => {
+  try {
+    const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`;
+    const data = await fetchData(WEATHER_API_URL);
+
+    const uniqueForecastDays = [];
+    const fiveDaysForecast = data.list.filter((forecast) => {
+      const forecastDate = new Date(forecast.dt_txt).getDate();
+      if (!uniqueForecastDays.includes(forecastDate)) {
+        return uniqueForecastDays.push(forecastDate);
+      }
+    });
+
+    // Limpiar contenido previo
+    cityInput.value = '';
+    currentWeatherDiv.innerHTML = '';
+    weatherCardsDiv.innerHTML = '';
+
+    // Insertar tarjetas de clima
+    fiveDaysForecast.forEach((weatherItem, index) => {
+      if (index === 0) {
+        currentWeatherDiv.insertAdjacentHTML(
+          'beforeend',
+          createWeatherCard(cityName, weatherItem, index)
+        );
+      } else {
+        weatherCardsDiv.insertAdjacentHTML(
+          'beforeend',
+          createWeatherCard(cityName, weatherItem, index)
+        );
+      }
+    });
+  } catch (error) {
+    // Manejar errores
+    console.error('Error fetching weather data:', error.message);
+    alert('An error occurred while fetching the weather forecast!');
+  }
+};
+
+// Función para obtener coordenadas de la ciudad ingresada por el usuario
+const getCityCoordinates = async () => {
+  try {
+    const cityName = cityInput.value.trim();
+    if (!cityName) return;
+    const GEOCODING_API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&units=metric&appid=${API_KEY}`;
+
+    const data = await fetchData(GEOCODING_API_URL);
+
+    if (!data.length) return;
+    const { name, lat, lon } = data[0];
+    await getWeatherDetails(name, lat, lon);
+  } catch (error) {
+    // Manejar errores
+    console.error('Error fetching coordinates:', error.message);
+    alert('An error occurred while fetching the coordinates!');
+  }
+};
+
+// Función para obtener coordenadas del usuario actual
+const getUserCoordinates = async () => {
+  try {
+    const position = await new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+
+    const { latitude, longitude } = position.coords;
+    const REVERSE_GEOCODING_URL = `http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&units=metric&appid=${API_KEY}`;
+
+    const data = await fetchData(REVERSE_GEOCODING_URL);
+
+    const { name } = data[0];
+    await getWeatherDetails(name, latitude, longitude);
+  } catch (error) {
+    // Manejar errores
+    console.error('Error fetching coordinates:', error.message);
+    alert('An error occurred while fetching the city!');
+  }
+};
+
+// Event Listeners
+locationButton.addEventListener('click', getUserCoordinates);
+searchButton.addEventListener('click', getCityCoordinates);
+cityInput.addEventListener(
+  'keyup',
+  (e) => e.key === 'Enter' && getCityCoordinates()
+);
